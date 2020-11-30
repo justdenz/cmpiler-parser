b7cc7131b7fd0e0132bc551967ad0753b5f7be6b
Denzel Co <denz3550@gmail.com>
2020-11-30 17:12:35 +0800

    setup class files

--- main.java
+++ main.java
@@ -1,13 +1,24 @@
 package parser;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.TokenStream;
-import parser.CParser;
+import parser.*;
 
-public class main{
+public class Main{
     public static void main(String[] args){
-
+        try{
+            String test = "System.out.println(1)";
+            InputStream inputStream = Main.class.getResourceAsStream(test);
+            Lexer lexer = new CLexer(CharStreams.fromStream(inputStream));
+            TokenStream tokenStream = new CommonTokenStream(lexer);
+            CParser parser = new CParser(tokenStream);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
