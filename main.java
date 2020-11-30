08de21bd58c11b0962a035794b581e1e8c3ddadc
Denzel Co <denz3550@gmail.com>
2020-11-30 15:26:06 +0800

    set up packages

--- main.java
+++ main.java
@@ -5,6 +5,7 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.TokenStream;
 import parser.CParser;
+
 public class main{
     public static void main(String[] args){
 
