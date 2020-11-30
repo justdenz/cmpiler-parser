77120d2ce5bbd4e7e93ab7567bc1ebea1878a342
Denzel Co <denz3550@gmail.com>
2020-11-30 14:13:53 +0800

    set up folders

---
+++ main.java
@@ -0,0 +1,12 @@
+package grammar;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.TokenStream;
+import grammar.CParser;
+public class main{
+    public static void main(String[] args){
+
+    }
+}
