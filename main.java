0680cf8f7e033f4479fc1889c0c98b528f26ff6d
Denzel Co <denz3550@gmail.com>
2020-11-30 17:56:49 +0800

    running

--- main.java
+++ main.java
@@ -7,18 +7,19 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.TokenStream;
-import parser.*;
+
+import parser.CParser;
 
 public class Main{
     public static void main(String[] args){
-        try{
-            String test = "System.out.println(1)";
-            InputStream inputStream = Main.class.getResourceAsStream(test);
-            Lexer lexer = new CLexer(CharStreams.fromStream(inputStream));
-            TokenStream tokenStream = new CommonTokenStream(lexer);
-            CParser parser = new CParser(tokenStream);
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
+        // try{
+        //     String test = "System.out.println(1)";
+        //     InputStream inputStream = Main.class.getResourceAsStream(test);
+        //     Lexer lexer = new CLexer(CharStreams.fromStream(inputStream));
+        //     TokenStream tokenStream = new CommonTokenStream(lexer);
+        //     CParser parser = new CParser(tokenStream);
+        // }catch (IOException e) {
+        //     e.printStackTrace();
+        // }
     }
 }
