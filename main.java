73a8038ec680e9630d0d739138a56f8505881a27
Denzel Co <denz3550@gmail.com>
2020-11-30 15:03:58 +0800

    setup packages

--- main.java
+++ main.java
@@ -1,10 +1,10 @@
-package grammar;
+package parser;
 
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.TokenStream;
-import grammar.CParser;
+import parser.CParser;
 public class main{
     public static void main(String[] args){
 
