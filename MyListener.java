5325a01b625b8078345b5ff53dd9e8ebbd2eaaa9
Denzel Co <denz3550@gmail.com>
2020-12-07 17:34:46 +0800

    implemented listeners

---
+++ MyListener.java
@@ -0,0 +1,9 @@
+import parser.CBaseListener;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+
+public class MyListener extends CBaseListener {
+    @Override public void enterEveryRule(ParserRuleContext ctx) {  //see gramBaseListener for allowed functions
+        System.out.println("rule entered: " + ctx.getText());      //code that executes per rule
+    }
+}
--- main.java
+++ main.java
@@ -5,17 +5,23 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import parser.CParser;
 import parser.CLexer;
 
 class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         try{
             InputStream inputStream = Main.class.getResourceAsStream("input.txt");
             Lexer lexer = new CLexer(CharStreams.fromStream(inputStream));
             TokenStream tokenStream = new CommonTokenStream(lexer);
             CParser parser = new CParser(tokenStream);
+            CParser.CompilationUnitContext tree = parser.compilationUnit();
+            MyListener listener = new MyListener();
+
+            ParseTreeWalker walker = new ParseTreeWalker();
+            walker.walk(listener,tree);
         }catch (IOException e) {
             e.printStackTrace();
         }
