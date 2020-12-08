049a1477d749a3d6b4c0f19ea7d0e45bf3632f5b
schuyl3r <schuyler_ng@dlsu.edu.ph>
2020-12-08 18:05:13 +0800

    feat: updated bat and sh file

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-java -jar lib/antlr-4.9-complete.jar -package parser -o parser C.g4
+java -jar lib/antlr-4.9-complete.jar -package parser -o parser CUSTOM.g4
 javac -cp ".;lib/antlr-4.9-complete.jar" *.java
 java -cp ".;lib/antlr-4.9-complete.jar" Main
