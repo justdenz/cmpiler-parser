477e3f7d588983df5f62ba361b84ec0a3ea3b102
Rae <raeanne1999@gmail.com>
2020-12-11 01:56:23 +0800

    added MainGUI

--- main.java
+++ main.java
@@ -1,35 +1,38 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
-
+import javafx.application.Application;
 import org.antlr.v4.gui.TreeViewer;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.tree.*;
-
-import parser.CUSTOMParser;
 import parser.CUSTOMLexer;
+import parser.CUSTOMParser;
 
-class Main{
-    public static void main(String[] args) throws Exception{
-        try{
-            InputStream inputStream = Main.class.getResourceAsStream("input.txt");
-            Lexer lexer = new CUSTOMLexer(CharStreams.fromStream(inputStream));
-            TokenStream tokenStream = new CommonTokenStream(lexer);
-            CUSTOMParser parser = new CUSTOMParser(tokenStream);
-            parser.removeErrorListeners();
-            parser.addErrorListener(MyListener.INSTANCE);
-            ParseTree tree = parser.program();
+class Main {
 
-            TreeViewer viewer = new TreeViewer(Arrays.asList(parser.getRuleNames()), tree);
-            viewer.open();
+  public static void main(String[] args) throws Exception {
+    try {
+      Application.launch(MainGUI.class, args);
+      InputStream inputStream = Main.class.getResourceAsStream("input.txt");
+      Lexer lexer = new CUSTOMLexer(CharStreams.fromStream(inputStream));
+      TokenStream tokenStream = new CommonTokenStream(lexer);
+      CUSTOMParser parser = new CUSTOMParser(tokenStream);
+      parser.removeErrorListeners();
+      parser.addErrorListener(MyListener.INSTANCE);
+      ParseTree tree = parser.program();
 
-            // ParseTreeWalker walker = new ParseTreeWalker();
-            // walker.walk(listener,tree);
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
+      TreeViewer viewer = new TreeViewer(
+        Arrays.asList(parser.getRuleNames()),
+        tree
+      );
+      //   viewer.open();
+      // ParseTreeWalker walker = new ParseTreeWalker();
+      // walker.walk(listener,tree);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 }
--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 java -jar lib/antlr-4.9-complete.jar -package parser -o parser CUSTOM.g4
-javac -cp ".;lib/antlr-4.9-complete.jar" *.java
-java -cp ".;lib/antlr-4.9-complete.jar" Main
+javac --module-path lib\javafx-sdk-11.0.2\lib --add-modules javafx.controls -cp ";lib/richtextfx-fat-0.10.5.jar;lib/antlr-4.9-complete.jar"  *.java
+java --module-path lib\javafx-sdk-11.0.2\lib --add-modules javafx.controls -cp ";lib/richtextfx-fat-0.10.5.jar;lib/antlr-4.9-complete.jar" Main
