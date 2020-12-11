d8f0f312158f5998d89830c6fca19cb164eaeb16
schuyl3r <schuyler_ng@dlsu.edu.ph>
2020-12-11 18:42:28 +0800

    wip: updating sh file for javafx

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # Compile g4 file
 java -jar lib/antlr-4.9-complete.jar -package parser -o parser CUSTOM.g4
 # Compile All Java Files (Replace * to compile specific file)
-javac -cp ".:lib/antlr-4.9-complete.jar" *.java
+javac --module-path lib/javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/richtextfx-fat-0.10.5.jar:lib/antlr-4.9-complete.jar" *.java
 # Run Main
-java -cp ".:lib/antlr-4.9-complete.jar" Main
+java --module-path lib/javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/richtextfx-fat-0.10.5.jar:lib/antlr-4.9-complete.jar" Main
