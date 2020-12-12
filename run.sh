4f40c40da10ddfe326183c861dcb2f5078cf2410
schuyl3r <schuyler_ng@dlsu.edu.ph>
2020-12-12 11:34:15 +0800

    fix: updated sh file for MVC

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # Compile g4 file
 java -jar lib/antlr-4.9-complete.jar -package model -o model CUSTOM.g4
 # Compile All Java Files (Replace * to compile specific file)
-javac -cp ".:lib/antlr-4.9-complete.jar" *.java
-# Run Main
-java -cp ".:lib/antlr-4.9-complete.jar" Main
+javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/richtextfx-fat-0.10.5.jar:lib/antlr-4.9-complete.jar" *.java
+# Run App
+java --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/richtextfx-fat-0.10.5.jar:lib/antlr-4.9-complete.jar" app
