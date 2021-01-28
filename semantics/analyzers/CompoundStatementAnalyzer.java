c8a3ad6a4bf10c960958fa11efa4df17b0a8ee44
Denzel Co <denz3550@gmail.com>
2021-01-29 01:08:03 +0800

    compound statement analyazer skeleton

--- semantics/analyzers/CompoundStatementAnalyzer.java
+++ semantics/analyzers/CompoundStatementAnalyzer.java
@@ -1,7 +1,11 @@
 package semantics.analyzers;
 
+import java.util.List;
+
 import model.CUSTOMParser.CompoundStatementContext;
+import model.CUSTOMParser.CompoundStatementListContext;
 import semantics.symboltable.GlobalScopeManager;
+import semantics.symboltable.scopes.CstmLocalScope;
 
 public class CompoundStatementAnalyzer {
     public CompoundStatementAnalyzer(){
@@ -9,6 +13,19 @@ public class CompoundStatementAnalyzer {
     }
 
     public void analyze(CompoundStatementContext ctx){
+        List<CompoundStatementListContext> compoundStatementListContext = ctx.compoundStatementList();
+
+        for(CompoundStatementListContext compoundStatementContext: compoundStatementListContext){
+            if(compoundStatementContext.statementList() != null){
+
+            }else if(compoundStatementContext.declarationList() != null){
+
+            }
+        }
+
         
+        CstmLocalScope parentScope = GlobalScopeManager.getInstance().getCurrentScope().getParent();
+        GlobalScopeManager.getInstance().setCurrentScope(parentScope);
     }
+
 }
