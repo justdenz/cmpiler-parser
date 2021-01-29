2896bb81af3f2407ae19bfd4e818a09d60529cdc
schuyl3r <schuyler_ng@dlsu.edu.ph>
2021-01-29 19:35:35 +0800

    updated sh file

--- run.sh
+++ run.sh
@@ -3,8 +3,16 @@ java -jar lib/antlr-4.9-complete.jar -package model -o model CUSTOM.g4
 # Compile All Java Files (Replace * to compile specific file)
 javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/antlr-4.9-complete.jar"  model/*.java
 javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/antlr-4.9-complete.jar"  controller/*.java
+javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/antlr-4.9-complete.jar"  builder/errorcheckers/*.java
+javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/antlr-4.9-complete.jar"  builder/*.java
+javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/antlr-4.9-complete.jar"  console/*.java
 javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/richtextfx-fat-0.10.5.jar"  view/*.java
 javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/richtextfx-fat-0.10.5.jar"  components/*.java
+javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/antlr-4.9-complete.jar"  semantics/analyzers/*.java
+javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/antlr-4.9-complete.jar"  semantics/representations/*.java
+javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/antlr-4.9-complete.jar"  semantics/symboltable/scopes/*.java
+javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/antlr-4.9-complete.jar"  semantics/symboltable/*.java
+javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/antlr-4.9-complete.jar"  semantics/utils/*.java
 javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/richtextfx-fat-0.10.5.jar:lib"  app.java
 # javac --module-path lib/mac-javafx-sdk-11.0.2/lib --add-modules javafx.controls -cp ".:lib/richtextfx-fat-0.10.5.jar:lib/antlr-4.9-complete.jar" *.java
 # Run App
