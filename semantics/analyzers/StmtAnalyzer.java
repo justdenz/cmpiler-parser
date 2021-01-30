45ce4e521f3e04f000c6d6312454084a07c2922b
Denzel Co <denz3550@gmail.com>
2021-01-30 17:18:50 +0800

    removed unnecessary print statements

--- semantics/analyzers/StmtAnalyzer.java
+++ semantics/analyzers/StmtAnalyzer.java
@@ -62,7 +62,6 @@ public class StmtAnalyzer{
                 // verify the declared variables in condition
                 CstmUnDecChecker undecChecker = new CstmUnDecChecker(selectStmtCtx.simpleExpression());
                 undecChecker.verify();
-                System.out.println(selectStmtCtx.simpleExpression().getText());
                 CstmLocalScope ifScope = new CstmLocalScope(GlobalScopeManager.getInstance().getCurrentScope());
                 GlobalScopeManager.getInstance().setCurrentScope(ifScope);
                 System.out.println("Opened if/else if scope");
