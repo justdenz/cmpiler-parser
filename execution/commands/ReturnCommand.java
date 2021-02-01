d8fdff73ddc2010ac5a89c37d5de51e3eabd48a8
schuyl3r <schuyler_ng@dlsu.edu.ph>
2021-02-01 20:38:01 +0800

    feat: added converting of big decimal to prim value in return command

--- execution/commands/ReturnCommand.java
+++ execution/commands/ReturnCommand.java
@@ -1,11 +1,14 @@
 package execution.commands;
 
+import java.math.BigDecimal;
+
 import builder.errorcheckers.CstmTypeChecker;
 import builder.errorcheckers.CstmUnDecChecker;
 import console.Printer;
 import model.CUSTOMParser.SimpleExpressionContext;
 import semantics.representations.CstmFunction;
 import semantics.representations.CstmValue;
+import semantics.representations.CstmValue.PrimitiveType;
 import semantics.symboltable.GlobalScopeManager;
 import semantics.symboltable.scopes.CstmLocalScope;
 
@@ -14,7 +17,7 @@ public class ReturnCommand implements CommandInterface{
   private SimpleExpressionContext simpleExpCtx;
   private CstmFunction cstmFunc;
   private CstmLocalScope cstmLocalScope;
-
+  private EvaluationCommand evaluationCommand;
 
   public ReturnCommand(SimpleExpressionContext simpExpCtx, CstmFunction cstmFunc){
     this.simpleExpCtx = simpExpCtx;
@@ -28,15 +31,32 @@ public class ReturnCommand implements CommandInterface{
     CstmTypeChecker typeChecker = new CstmTypeChecker(cstmVal, this.simpleExpCtx);
     typeChecker.verify();
 
+    evaluationCommand = new EvaluationCommand(this.simpleExpCtx, this.cstmLocalScope);
   }
 
 	@Override
 	public void execute() {
-    EvaluationCommand evaluationCommand = new EvaluationCommand(this.simpleExpCtx, this.cstmLocalScope);
     evaluationCommand.execute();
-
     CstmValue cstmValue = this.cstmFunc.getReturnValue();
-    Printer.getInstance().display(cstmValue.toString());
+    BigDecimal evalResult = evaluationCommand.getResult();
+
+      if(cstmValue != null){
+        if(cstmValue.getPrimitiveType() == PrimitiveType.INT){
+            cstmValue.setValue(evalResult.intValue());
+        } else if(cstmValue.getPrimitiveType() == PrimitiveType.FLOAT){
+            cstmValue.setValue(evalResult.floatValue());
+        } else if(cstmValue.getPrimitiveType() == PrimitiveType.STRING){
+            cstmValue.setValue(simpleExpCtx.getText().replaceAll("\"", ""));
+        } else if(cstmValue.getPrimitiveType() == PrimitiveType.BOOLEAN){
+            switch(evalResult.intValue()){
+                case 0:
+                    cstmValue.setValue(false);
+                    break;
+                case 1:
+                    cstmValue.setValue(true);
+                    break;
+            }
+        }
+      }
 	}
-  
 }
