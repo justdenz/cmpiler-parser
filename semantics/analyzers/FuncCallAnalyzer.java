5040ae02e76f53277635a74c0737b28a2a98f9c7
schuyl3r <schuyler_ng@dlsu.edu.ph>
2021-02-01 22:26:37 +0800

    feat: paramnum and func call analyzer

---
+++ semantics/analyzers/FuncCallAnalyzer.java
@@ -0,0 +1,38 @@
+package semantics.analyzers;
+
+import java.util.List;
+
+import builder.errorcheckers.CstmUnDecChecker;
+import model.CUSTOMParser.ArgsContext;
+import model.CUSTOMParser.CallContext;
+import model.CUSTOMParser.SimpleExpressionContext;
+import semantics.representations.CstmFunction;
+import semantics.symboltable.GlobalScopeManager;
+
+public class FuncCallAnalyzer {
+  
+  private CallContext callCtx;
+  private ArgsContext argsCtx;
+  private CstmFunction function;
+  private String functionCallName;
+
+  public FuncCallAnalyzer(CallContext callCtx){
+    this.callCtx = callCtx;
+    this.argsCtx = callCtx.args();
+    this.functionCallName = callCtx.IDENTIFIER().getText();
+    this.function = GlobalScopeManager.getInstance().getFunction(this.functionCallName);
+  }
+
+  public void analyze(){
+    ParamNumAnalyzer paramNumAnalyzer = new ParamNumAnalyzer(this.function, this.argsCtx);
+    paramNumAnalyzer.analyze();
+
+    List<SimpleExpressionContext> callArgsList = this.argsCtx.simpleExpression();
+
+    for(SimpleExpressionContext exprCtx : callArgsList){
+      CstmUnDecChecker unDecChecker = new CstmUnDecChecker(exprCtx);
+      unDecChecker.verify();
+    }
+  }
+
+}
---
+++ semantics/analyzers/ParamNumAnalyzer.java
@@ -0,0 +1,32 @@
+package semantics.analyzers;
+
+import java.util.List;
+
+import console.Console;
+import model.CUSTOMParser.ArgsContext;
+import model.CUSTOMParser.SimpleExpressionContext;
+import semantics.representations.CstmFunction;
+
+public class ParamNumAnalyzer {
+  
+  private CstmFunction function;
+  private int lineNumber;
+  private List<SimpleExpressionContext> exprCtxList;
+ 
+
+  public ParamNumAnalyzer(CstmFunction function, ArgsContext argsCtx){
+    this.function = function;
+    this.lineNumber = argsCtx.getStart().getLine();
+    if(argsCtx.simpleExpression() != null){
+      this.exprCtxList = argsCtx.simpleExpression();
+    }
+  }
+
+  public void analyze(){
+    if(this.exprCtxList == null && this.function.getParameterCount() != 0){
+      Console.log(String.valueOf(this.lineNumber), "Found a paramater count mismatch.");
+    } else if(this.exprCtxList == null && this.function.getParameterCount() != this.exprCtxList.size()){
+      Console.log(String.valueOf(this.lineNumber), "Found a paramater count mismatch.");
+    }
+  }
+}
