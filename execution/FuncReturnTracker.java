bb4e5e8ecbdca875e3e7aea50d60c735392516dd
schuyl3r <schuyler_ng@dlsu.edu.ph>
2021-02-03 20:55:41 +0800

    feat: function command tracker and return tracker

---
+++ execution/FuncReturnTracker.java
@@ -0,0 +1,45 @@
+package execution;
+
+import semantics.representations.CstmFunction;
+
+public class FuncReturnTracker {
+  
+  private static FuncReturnTracker sharedInstance = null;
+  private CstmFunction currentFunction;
+  private boolean hasReturned;
+
+  public FuncReturnTracker(){
+    currentFunction = null;
+    hasReturned = false;
+  }
+
+  public static FuncReturnTracker getInstance(){
+    if(sharedInstance == null){
+      sharedInstance = new FuncReturnTracker();
+      System.out.println("Function Return Tracker initialized");
+    }
+    return sharedInstance;
+  }
+
+  public void reset(){
+    sharedInstance = null;
+    hasReturned = false;
+    currentFunction = null;
+  }
+
+  public CstmFunction getCurFunction(){
+    return currentFunction;
+  }
+
+  public void setCurFunction(CstmFunction cstmFunction){
+    this.currentFunction = cstmFunction;
+  }
+
+  public boolean funcHasReturned(){
+    return hasReturned;
+  }
+
+  public void setFuncReturned(boolean hasReturned){
+    this.hasReturned = hasReturned;
+  }
+}
